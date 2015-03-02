afa452537e0904fc9a030f18b2e77071c514abf0
ajchang <ajchang@porygon.cs.uchicago.edu>
2015-03-02 14:46:59 -0600

    Initial Commit

---
+++ GratitudeJournal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GratitudeJournal
+//
+//  Created by ajchang on 3/2/15.
+//  Copyright (c) 2015 ajchang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GratitudeJournal/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GratitudeJournal
+//
+//  Created by ajchang on 3/2/15.
+//  Copyright (c) 2015 ajchang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
