97501e82eaf5c6165b8f2fd5cb8785e922504197
ajchang <ajchang@ditto.cs.uchicago.edu>
2015-03-14 14:27:35 -0500

    resized filter scroll view images to 100x100, fixed scroll view width, cut down on filters

--- GratitudeJournal/FilterImageViewController.m
+++ GratitudeJournal/FilterImageViewController.m
@@ -21,12 +21,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.filterScrollView.contentSize = CGSizeMake(1450, 100);
+    self.filterScrollView.contentSize = CGSizeMake(900, 100);
     self.filterImgViews = [[NSMutableArray alloc] initWithCapacity:8];
-    self.filters = [NSArray arrayWithObjects:@"Original", @"CILinearToSRGBToneCurve", @"CIPhotoEffectChrome", @"CIPhotoEffectFade", @"CIPhotoEffectInstant", @"CIPhotoEffectMono", @"CIPhotoEffectNoir", @"CIPhotoEffectProcess", @"CIPhotoEffectTonal", @"CIPhotoEffectTransfer", @"CISRGBToneCurveToLinear", @"CIVignetteEffect", nil];
+    self.filters = [NSArray arrayWithObjects:@"CILinearToSRGBToneCurve", @"CIPhotoEffectChrome", @"CIPhotoEffectFade", @"CIPhotoEffectInstant", @"CIPhotoEffectNoir", @"CIPhotoEffectProcess", @"CIPhotoEffectTransfer", @"CISRGBToneCurveToLinear", @"CIVignetteEffect", nil];
     
     //@@@why does this take so long to load?!!
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 9; i++) {
+        NSLog(@"filter %d: %@", i, [self.filters objectAtIndex:i]);
         //create raw CIImage
         CIImage *rawImageData = [[CIImage alloc] initWithImage:[UIImage imageNamed:@"sample"]];
         
@@ -39,14 +40,25 @@
         CIImage *filteredImgData = [filter valueForKey:@"outputImage"];
         //create filtered UIImage with filtered CIImage
         UIImage *filteredImg = [UIImage imageWithCIImage:filteredImgData];
-        //filterImgView.image = filteredImg;
+        //UIImageView *filterImgView = [[UIImageView alloc]initWithFrame:CGRectMake(i*100, 0, 100, 100)];
+        //filterImgView.contentMode = UIViewContentModeScaleAspectFit;
+        //filterImgView.clipsToBounds = YES;
+        UIImage *filteredImgResized = [self imageWithImage:filteredImg scaledToSize:CGSizeMake(100, 100)];
+        
+        
+        //[filterImgView setImage:filteredImgResized];
+        //[self.filterScrollView addSubview:filterImgView];
+        
+        //NSLog(@"image height: %f", filterImgView.image.size.height);
         
         //create a button to overlay over the frame of the image
         UIButton *filterButton = [[UIButton alloc] initWithFrame:CGRectMake(i*100, 0, 100, 100)];
         filterButton.tag = i + 100;
         [filterButton addTarget:self action:@selector(chooseFilter:) forControlEvents:UIControlEventTouchUpInside];
-        [filterButton setBackgroundImage:filteredImg forState:UIControlStateNormal];
+        [filterButton setBackgroundImage:filteredImgResized forState:UIControlStateNormal];
         [self.filterScrollView addSubview:filterButton];
+        
+        NSLog (@"button image height: %f", filterButton.currentBackgroundImage.size.height);
     }
 }
 
@@ -56,6 +68,19 @@
 }
 
 #pragma mark - Filter Actions
+
+//http://ajourneywithios.blogspot.com/2012/03/resizing-uiimage-in-ios.html
+- (UIImage*)imageWithImage:(UIImage*)image
+scaledToSize:(CGSize)newSize
+{
+    UIGraphicsBeginImageContext( newSize );
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 - (IBAction)chooseFilter:(UIButton *)sender {
     NSLog(@"chose filter %lu", sender.tag - 100);
 }
